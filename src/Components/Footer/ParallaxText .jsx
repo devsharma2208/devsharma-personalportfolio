2563b390cb3374fd4780494bc177cdbffa5f46f1
Dev Sharma <devsharmaelc@gmail.com>
2024-04-10 18:33:15 +0530

    add font size

--- src/Components/Footer/ParallaxText .jsx
+++ src/Components/Footer/ParallaxText .jsx
@@ -23,21 +23,12 @@ const ParallaxText = ({ children, baseVelocity = 100 }) => {
     clamp: false,
   });
 
-  /**
-   * This is a magic wrapping for the length of the text - you
-   * have to replace for wrapping that works for you or dynamically
-   * calculate
-   */
   const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
 
   const directionFactor = useRef(1);
   useAnimationFrame((t, delta) => {
     let moveBy = directionFactor.current * baseVelocity * (delta / 4000);
 
-    /**
-     * This is what changes the direction of the scroll once we
-     * switch scrolling directions.
-     */
     if (velocityFactor.get() < 0) {
       directionFactor.current = -1;
     } else if (velocityFactor.get() > 0) {
@@ -49,13 +40,6 @@ const ParallaxText = ({ children, baseVelocity = 100 }) => {
     baseX.set(baseX.get() + moveBy);
   });
 
-  /**
-   * The number of times to repeat the child text should be dynamically calculated
-   * based on the size of the text and viewport. Likewise, the x motion value is
-   * currently wrapped between -20 and -45% - this 25% is derived from the fact
-   * we have four children (100% / 4). This would also want deriving from the
-   * dynamically generated number of children.
-   */
   return (
     <div className="parallax">
       <motion.div className="scroller" style={{ x }}>
